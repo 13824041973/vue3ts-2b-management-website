fcc5cd6335bcc80163fad5f4c63462c10f117010
LuyolG <1402098743@qq.com>
2024-12-28 04:23:29 +0800

    feat:集成vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="app">
+    <RouterLink to="/login">登录</RouterLink>
+    <RouterLink to="/main">首页</RouterLink>
+    <RouterView />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 </script>
 
